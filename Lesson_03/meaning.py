b72c5b877af2ca699eff17cb5d90d622f57fc041
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-11-16 13:59:14 -0600

    added lessons 3 and 4

---
+++ Lesson_03/meaning.py
@@ -0,0 +1,12 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('right on')
+#     print('')
+# else:
+#     print('not today')
+#     print('')
+
+# Ternary operator
+print('Right on!') if meaning > 10 else print('Not greater')
---
+++ Lesson_04/data_types.py
@@ -0,0 +1,18 @@
+# String Data Type
+
+# Literal Assignment
+first = 'Dr'
+last = 'Gonzo'
+
+# print('')
+# print(type(first))
+# print(type(first) == int)
+# print(isinstance(first, str))
+# print('')
+
+# Constructor Function
+pizza = str('Pepperoni')
+print(type(pizza))
+print(type(pizza) == bool)
+print(isinstance(pizza, str))
+print('')
