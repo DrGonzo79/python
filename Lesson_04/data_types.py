8fd56552c790c442051c71e4966fa94af68bf7e4
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-11-21 11:08:38 -0600

    Added string methods to L4

--- Lesson_04/data_types.py
+++ Lesson_04/data_types.py
@@ -1,18 +1,74 @@
-# String Data Type
+# # String Data Type
 
-# Literal Assignment
-first = 'Dr'
-last = 'Gonzo'
+# # Literal Assignment
+# first = 'Dr'
+# last = 'Gonzo'
+
+# # print('')
+# # print(type(first))
+# # print(type(first) == int)
+# # print(isinstance(first, str))
+# # print('')
+
+# # Constructor Function
+# # pizza = str('Pepperoni')
+# # print(type(pizza))
+# # print(type(pizza) == bool)
+# # print(isinstance(pizza, str))
+# # print('')
+
+# # Concatenation
+# fullname = first + " " + last
+# print(fullname)
+
+# fullname += '!'
+# print(fullname)
+
+# # Casting a number to a string
+# decade = str(1980)
+# print(type(decade))
+# print(decade)
+
+# statement = "I like music from the " + decade + "s."
+# print(statement)
+
+# # Multiple lines
+# multiline = '''
+# Hey how are you.
+
+# I was just checking in.
+#             All good?
+# '''
+
+# print(multiline)
+
+# # Escaping special characters
+# sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+
+# print(sentence)
+
+# # String methods
+# print(first)
+# print(first.lower())
+# print(first.upper())
+# print(first)
+
+# print(multiline.title())
+# print(multiline.replace("good", 'ok'))
+
+# print(len(multiline))
+# multiline += "                             "
+# multiline = "              " + multiline
+# print(len(multiline))
 
 # print('')
-# print(type(first))
-# print(type(first) == int)
-# print(isinstance(first, str))
-# print('')
+# print(len(multiline.strip()))
+# print(len(multiline.rstrip()))
+# print(len(multiline.lstrip()))
 
-# Constructor Function
-pizza = str('Pepperoni')
-print(type(pizza))
-print(type(pizza) == bool)
-print(isinstance(pizza, str))
+# Build a menu
+title = "menu".upper()
+print('')
+print(title.center(20, "="))
+print("Coffee".ljust(16, ".") + "$1".rjust(4))
 print('')
