b5c043f59a92096ca68e64655ec2b9de4771584e
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-12-15 17:09:05 -0500

    updated loops and rps2

--- Lesson_08/loops.py
+++ Lesson_08/loops.py
@@ -7,10 +7,46 @@ while value <= 10:
 
 print("***********************")
 
-while value <= 10:
-    value += 1
-    if value == 1:
-        continue
-    print(value)
-else:
-    print("Value is now equal to " + str(value))
+# while value <= 10:
+#     value += 1
+#     if value == 1:
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to " + str(value))
+
+names = ["John", "Mary", "Bob"]
+# for x in names:
+#     print(x)
+
+# for x in "Mississippi":
+#     print(x)
+
+# for x in names:
+#     if x == "Mary":
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "Mary":
+#         continue
+#     print(x)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(5, 101, 5):
+#     print(x)
+# else:
+#     print("Done son!")
+
+names = ["John", "Mary", "Bob"]
+actions = ["ran", "ate", "walked"]
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action)
+
+for action in actions:
+    for name in names:
+        print(name + " " + action)
---
+++ Lesson_08/rps2.py
@@ -0,0 +1,48 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+playagain = True
+
+while playagain:
+
+    playerChoice = input(
+        '\nEnter... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n')
+    player = int(playerChoice)  # cast str to int
+
+    if player < 1 or player > 3:
+        sys.exit('You did not enter a number between 1-3, try again!\n')
+
+    computerChoice = random.choice('123')
+    computer = int(computerChoice)
+
+    print('\nYou chose ' + str(RPS(player)).replace('RPS.', '') + '!')
+    print('Python chose ' + str(RPS(computer)).replace('RPS.', '') + '!\n')
+
+    if player == 1 and computer == 3:
+        print('🥳 You win!\n')
+    elif player == 2 and computer == 1:
+        print('🥳 You win!\n')
+    elif player == 3 and computer == 2:
+        print('🥳 You win!\n')
+    elif player == computer:
+        print('🤯 You tied Python!\n')
+    else:
+        print('🐍 Python wins!\n')
+
+    playagain = input("\nPlay again? \nY for Yes, N for No: \n")
+
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\nThanks for playing!")
+        playagain = False
+
+sys.exit("Bye! 👋🏻")
